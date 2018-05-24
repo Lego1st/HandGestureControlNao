aae0546b5ab5d506a6d425ea275872ca7295fbb2
Nguyen Trung Nghia <nguyentrungnghia1812@gmail.com>
2018-05-24 14:58:48 +0700

    Clean code, rm skin model

--- main.py
+++ main.py
@@ -3,14 +3,8 @@ import numpy as np
 import os
 import time
 
-# import gestureCNN as myNN
 from utils import GestureModel
-from mxnet_mtcnn_face_detection import MtcnnDetector
-import mxnet as mx
-import urllib
 
-detector = MtcnnDetector(model_folder='mxnet_mtcnn_face_detection/model', ctx=mx.cpu(0), num_worker = 4 , accurate_landmark = False)
-# url='http://192.168.1.31:8080/shot.jpg'
 minValue = 70
 
 x0 = 200
@@ -76,62 +70,26 @@ detectCheckpoint = 30
 
 def skinMask(frame, x0, y0, width, height ):
     global guessGesture, visualize, mod, lastgesture, saveImg, y, b, r, hasFace
-    # framecounter = (framecounter + 1) % 30
-    # # HSV values
-
-    # if counter == 0:
-    #     img = cv2.resize(frame, (320,180))
-    #     results = detector.detect_face(img)
-    #     total_boxes = None
-    #     if results is not None:
-    #         total_boxes = results[0]
-
-        
-    #     frame_ycbcr = rgb2ycbcr(img)
-    #     hasFace = False
-    #     if total_boxes is not None and len(total_boxes) > 0:
-    #         # for box in total_boxes:
-    #         face = total_boxes[0]
-    #         hasFace = True
-    #         y, b, r = frame_ycbcr[int(face[1]) + int(face[3]-face[1])//2][int(face[0]) + int(face[2]-face[0])//2]
 
     cv2.rectangle(frame, (x0,y0),(x0+width,y0+height),(0,255,0),1)
     roi = frame[y0:y0+height, x0:x0+width]
     
 
-    ### Ycbcr
-    # roi = whiteBalance(roi)
     ycbcr = rgb2ycbcr(roi)
-    # low_range = np.array([8, 85, 135])
-    # upper_range = np.array([255, 135, 180])
+
     if not hasFace:
         y, b, r = ycbcr[height//2][width//2]
-        # print (y, b, r)
-    # y, b, r = 200, 126, 137
+
     radius = 30
     low_range = np.array([y-radius, b-radius, r-radius])
     upper_range = np.array([y+radius, b+radius, r+radius])
-    # low_range = np.array([200, 120, 130])
-    # upper_range = np.array([255, 130, 145])
     mask = cv2.inRange(ycbcr, low_range, upper_range)
-    ### HSV
 
-
-    # hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
-    
-    # h, s, v = (hsv[width//2][height//2])
-    # r = 30
-    # low_range = np.array([h-r, s-r, 0])
-    # upper_range = np.array([h+r, s+r, 150])
-    # mask = cv2.inRange(hsv, low_range, upper_range)
-    
-    # return mask
     mask = cv2.erode(mask, skinkernel, iterations = 1)
     mask = cv2.dilate(mask, skinkernel, iterations = 1)
     
     #blur
     mask = cv2.GaussianBlur(mask, (15, 15), 1)
-    #cv2.imshow("Blur", mask)
     
     #bitwise and mask original frame
     res = cv2.bitwise_and(roi, roi, mask = mask)
@@ -141,19 +99,11 @@ def skinMask(frame, x0, y0, width, height ):
     if saveImg == True:
         saveROIImg(res)
     elif guessGesture == True:
-        # retgesture = myNN.guessGesture(mod, res)
         retgesture = myNN.predict(res)
         if lastgesture != retgesture :
             lastgesture = retgesture
             controlNAO(lastgesture)
-            # print myNN.output[lastgesture]
             time.sleep(0.01)
-            #guessGesture = False
-    # elif visualize == True:
-    #     layer = int(raw_input("Enter which layer to visualize "))
-    #     cv2.waitKey(0)
-    #     myNN.visualizeLayers(mod, res, layer)
-    #     visualize = False
     
     
     return res
@@ -197,34 +147,6 @@ def Main():
     fy = 355
     fh = 18
     
-    #Call CNN model loading callback
-    # while True:
-    #     ans = int(raw_input( banner))
-    #     if ans == 2:
-    #         mod = myNN.loadCNN(-1)
-    #         myNN.trainModel(mod)
-    #         raw_input("Press any key to continue")
-    #         break
-    #     elif ans == 1:
-    #         print "Will load default weight file"
-    #         mod = myNN.loadCNN(0)
-    #         break
-    #     elif ans == 3:
-    #         if not mod:
-    #             w = int(raw_input("Which weight file to load (0 or 1)"))
-    #             mod = myNN.loadCNN(w)
-    #         else:
-    #             print "Will load default weight file"
-            
-    #         img = int(raw_input("Image number "))
-    #         layer = int(raw_input("Enter which layer to visualize "))
-    #         myNN.visualizeLayers(mod, img, layer)
-    #         raw_input("Press any key to continue")
-    #         continue
-        
-    #     else:
-    #         print "Get out of here!!!"
-    #         return 0
     global myNN
     myNN = GestureModel(input_shape = (64, 64, 1), weights = "bin_model_1.h5")
     ## Grab camera input
@@ -238,20 +160,12 @@ def Main():
     ret = cap.set(4,480)
     
     while(True):
-        # imgResp=urllib.urlopen(url)
-        # imgNp=np.array(bytearray(imgResp.read()),dtype=np.uint8)
-        # img=cv2.imdecode(imgNp,-1)
-        # ret, frame = True, img
         ret, frame = cap.read()
         max_area = 0
         
         frame = cv2.flip(frame, 3)
         
         if ret == True:
-            # if binaryMode == True:
-            #     roi = binaryMask(frame, x0, y0, width, height)
-            # else:
-            # roi = skinMask(frame, x0, y0, width, height)
             roi = binaryMask(frame, x0, y0, width, height)
 
         cv2.putText(frame,'Options:',(fx,fy), font, 0.7,(0,255,0),2,1)
@@ -288,11 +202,6 @@ def Main():
             guessGesture = not guessGesture
             print "Prediction Mode - {}".format(guessGesture)
         
-        ## This option is not yet complete. So disabled for now
-        ## Use v key to visualize layers
-        #elif key == ord('v'):
-        #    visualize = True
-
         ## Use i,j,k,l to adjust ROI window
         elif key == ord('i'):
             y0 = y0 - 5
@@ -312,13 +221,7 @@ def Main():
         ## numOfSamples controls number of snapshots to be taken PER gesture
         elif key == ord('s'):
             saveImg = not saveImg
-            
-            # if gestname != '':
-            #     saveImg = True
-            # else:
-            #     print "Enter a gesture group name first, by pressing 'n'"
-            #     saveImg = False
-        
+
         ## Use n key to enter gesture name
         elif key == ord('n'):
             gestname = raw_input("Enter the gesture folder name: ")
@@ -330,9 +233,6 @@ def Main():
                     print 'Some issue while creating the directory named -' + gestname
             
             path = "./"+gestname+"/"
-        
-        #elif key != 255:
-        #    print key
 
     #Realse & destroy
     cap.release()
